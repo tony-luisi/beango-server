f6f7ccba759d9d8551750a845c72fde58c7a8197
Tony Luisi <t.luisi@gmail.com>
2017-09-03 10:40:26 +1200

    added order items to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170902193225) do
+ActiveRecord::Schema.define(version: 20170902223822) do
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.string "name"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.time "order_time"
